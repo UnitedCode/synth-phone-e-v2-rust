4b9aa417482327f9cbd944a969624c7e2a3a3e7a
Nathan Bradshaw <nathansbradshaw@gmail.com>
2024-09-18 22:53:34 -0500

    chore: use std in test

--- autotune/src/tests.rs
+++ autotune/src/tests.rs
@@ -1,23 +1,21 @@
 #[cfg(test)]
 fn test() {
-use libm::sinf;
-use core::f64::consts::PI;
+    use std::f64::consts::PI;
 
+fn generate_sine_wave(frequency: f64, sample_rate: u32, duration: f64) -> Vec<f64> {
+    let num_samples = (sample_rate as f64 * duration) as usize;
+    let mut sine_wave = Vec::with_capacity(num_samples);
 
-fn integration_test() {
-    fn generate_sine_wave(frequency: f64, sample_rate: u32, duration: f64, buffer: &mut [f64]) -> usize {
-        let num_samples = core::cmp::min(buffer.len(), (sample_rate as f64 * duration) as usize);
-    
-        for n in 0..num_samples {
-            buffer[n] = sinf(2.0 * PI * frequency * n as f64 / sample_rate as f64);
-        }
-    
-        num_samples
+    for n in 0..num_samples {
+        let sample = (2.0 * PI * frequency * n as f64 / sample_rate as f64).sin();
+        sine_wave.push(sample);
     }
+
+    sine_wave
+}
     // TODO
     // Convert Sine to analysis Mag and Freq
     // Transform values
     // check that transformed values are what we expect
     // profit????????
-}
 }
