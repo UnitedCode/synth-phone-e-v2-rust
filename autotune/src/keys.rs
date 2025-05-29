1ae9c061f43205c992f04162894d2b1074360daf
Nathan Bradshaw <nathansbradshaw@gmail.com>
2025-05-28 21:38:39 -0500

    chore: cargo fmt

--- autotune/src/keys.rs
+++ autotune/src/keys.rs
@@ -1,73 +1,142 @@
-
-
 use crate::frequencies::*;
 
 /// A KeyScale is simply an array of 7 static string slices, e.g. ["C", "D", "E", "F", "G", "A", "B"].
 pub type KeyScaleFrequencies = [f32; 70];
-pub type KeyScale = ([&'static str; 7],KeyScaleFrequencies);
+pub type KeyScale = ([&'static str; 7], KeyScaleFrequencies);
 
 /// =======================================
 /// Major Key Scales
 /// =======================================
-pub const C_MAJOR_SCALE: KeyScale = (["C",  "D",  "E",  "F",  "G",  "A",  "B"], C_MAJOR_SCALE_FREQUENCIES);
-pub const G_MAJOR_SCALE: KeyScale = (["G",  "A",  "B",  "C",  "D",  "E",  "F#"], G_MAJOR_SCALE_FREQUENCIES);
-pub const D_MAJOR_SCALE: KeyScale = (["D",  "E",  "F#", "G",  "A",  "B",  "C#"], D_MAJOR_SCALE_FREQUENCIES);
-pub const A_MAJOR_SCALE: KeyScale = (["A",  "B",  "C#", "D",  "E",  "F#", "G#"], A_MAJOR_SCALE_FREQUENCIES);
-pub const E_MAJOR_SCALE: KeyScale = (["E",  "F#", "G#", "A",  "B",  "C#", "D#"], E_MAJOR_SCALE_FREQUENCIES);
-pub const B_MAJOR_SCALE: KeyScale = (["B",  "C#", "D#", "E",  "F#", "G#", "A#"], B_MAJOR_SCALE_FREQUENCIES);
-pub const F_SHARP_MAJOR_SCALE: KeyScale = (["F#", "G#", "A#", "B",  "C#", "D#", "E#"], FS_MAJOR_SCALE_FREQUENCIES);
-pub const C_SHARP_MAJOR_SCALE: KeyScale = (["C#", "D#", "E#", "F#", "G#", "A#", "B#"], CS_MAJOR_SCALE_FREQUENCIES);
-pub const F_MAJOR_SCALE: KeyScale = (["F",  "G",  "A",  "Bb", "C",  "D",  "E"], F_MAJOR_SCALE_FREQUENCIES);
-pub const BB_MAJOR_SCALE: KeyScale = (["Bb", "C",  "D",  "Eb", "F",  "G",  "A"], BB_MAJOR_SCALE_FREQUENCIES);
-pub const EB_MAJOR_SCALE: KeyScale = (["Eb", "F",  "G",  "Ab", "Bb", "C",  "D"], EB_MAJOR_SCALE_FREQUENCIES);
-pub const AB_MAJOR_SCALE: KeyScale = (["Ab", "Bb", "C",  "Db", "Eb", "F",  "G"], AB_MAJOR_SCALE_FREQUENCIES);
+pub const C_MAJOR_SCALE: KeyScale = (
+    ["C", "D", "E", "F", "G", "A", "B"],
+    C_MAJOR_SCALE_FREQUENCIES,
+);
+pub const G_MAJOR_SCALE: KeyScale = (
+    ["G", "A", "B", "C", "D", "E", "F#"],
+    G_MAJOR_SCALE_FREQUENCIES,
+);
+pub const D_MAJOR_SCALE: KeyScale = (
+    ["D", "E", "F#", "G", "A", "B", "C#"],
+    D_MAJOR_SCALE_FREQUENCIES,
+);
+pub const A_MAJOR_SCALE: KeyScale = (
+    ["A", "B", "C#", "D", "E", "F#", "G#"],
+    A_MAJOR_SCALE_FREQUENCIES,
+);
+pub const E_MAJOR_SCALE: KeyScale = (
+    ["E", "F#", "G#", "A", "B", "C#", "D#"],
+    E_MAJOR_SCALE_FREQUENCIES,
+);
+pub const B_MAJOR_SCALE: KeyScale = (
+    ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+    B_MAJOR_SCALE_FREQUENCIES,
+);
+pub const F_SHARP_MAJOR_SCALE: KeyScale = (
+    ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
+    FS_MAJOR_SCALE_FREQUENCIES,
+);
+pub const C_SHARP_MAJOR_SCALE: KeyScale = (
+    ["C#", "D#", "E#", "F#", "G#", "A#", "B#"],
+    CS_MAJOR_SCALE_FREQUENCIES,
+);
+pub const F_MAJOR_SCALE: KeyScale = (
+    ["F", "G", "A", "Bb", "C", "D", "E"],
+    F_MAJOR_SCALE_FREQUENCIES,
+);
+pub const BB_MAJOR_SCALE: KeyScale = (
+    ["Bb", "C", "D", "Eb", "F", "G", "A"],
+    BB_MAJOR_SCALE_FREQUENCIES,
+);
+pub const EB_MAJOR_SCALE: KeyScale = (
+    ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+    EB_MAJOR_SCALE_FREQUENCIES,
+);
+pub const AB_MAJOR_SCALE: KeyScale = (
+    ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+    AB_MAJOR_SCALE_FREQUENCIES,
+);
 
 /// =======================================
 /// Minor Key Scales (Natural Minor)
 /// =======================================
-pub const A_MINOR_SCALE: KeyScale = (["A",  "B",  "C",  "D",  "E",  "F",  "G"],A_MINOR_SCALE_FREQUENCIES);
-pub const E_MINOR_SCALE: KeyScale = (["E",  "F#", "G",  "A",  "B",  "C",  "D"],E_MINOR_SCALE_FREQUENCIES);
-pub const B_MINOR_SCALE: KeyScale = (["B",  "C#", "D",  "E",  "F#", "G",  "A"],B_MINOR_SCALE_FREQUENCIES);
-pub const F_SHARP_MINOR_SCALE: KeyScale = (["F#", "G#", "A",  "B",  "C#", "D",  "E"],FS_MINOR_SCALE_FREQUENCIES);
-pub const C_SHARP_MINOR_SCALE: KeyScale = (["C#", "D#", "E",  "F#", "G#", "A",  "B"],CS_MINOR_SCALE_FREQUENCIES);
-pub const G_SHARP_MINOR_SCALE: KeyScale = (["G#", "A#", "B",  "C#", "D#", "E",  "F#"],AB_MINOR_SCALE_FREQUENCIES);
-pub const D_MINOR_SCALE: KeyScale = (["D",  "E",  "F",  "G",  "A",  "Bb", "C"],D_MINOR_SCALE_FREQUENCIES);
-pub const G_MINOR_SCALE: KeyScale = (["G",  "A",  "Bb", "C",  "D",  "Eb", "F"],G_MINOR_SCALE_FREQUENCIES);
-pub const C_MINOR_SCALE: KeyScale = (["C",  "D",  "Eb", "F",  "G",  "Ab", "Bb"],C_MINOR_SCALE_FREQUENCIES);
-pub const F_MINOR_SCALE: KeyScale = (["F",  "G",  "Ab", "Bb", "C",  "Db", "Eb"],F_MINOR_SCALE_FREQUENCIES);
-pub const BB_MINOR_SCALE: KeyScale = (["Bb", "C",  "Db", "Eb", "F",  "Gb", "Ab"],BB_MINOR_SCALE_FREQUENCIES);
-pub const EB_MINOR_SCALE: KeyScale = (["Eb", "F",  "Gb", "Ab", "Bb", "Cb", "Db"],EB_MINOR_SCALE_FREQUENCIES);
+pub const A_MINOR_SCALE: KeyScale = (
+    ["A", "B", "C", "D", "E", "F", "G"],
+    A_MINOR_SCALE_FREQUENCIES,
+);
+pub const E_MINOR_SCALE: KeyScale = (
+    ["E", "F#", "G", "A", "B", "C", "D"],
+    E_MINOR_SCALE_FREQUENCIES,
+);
+pub const B_MINOR_SCALE: KeyScale = (
+    ["B", "C#", "D", "E", "F#", "G", "A"],
+    B_MINOR_SCALE_FREQUENCIES,
+);
+pub const F_SHARP_MINOR_SCALE: KeyScale = (
+    ["F#", "G#", "A", "B", "C#", "D", "E"],
+    FS_MINOR_SCALE_FREQUENCIES,
+);
+pub const C_SHARP_MINOR_SCALE: KeyScale = (
+    ["C#", "D#", "E", "F#", "G#", "A", "B"],
+    CS_MINOR_SCALE_FREQUENCIES,
+);
+pub const G_SHARP_MINOR_SCALE: KeyScale = (
+    ["G#", "A#", "B", "C#", "D#", "E", "F#"],
+    AB_MINOR_SCALE_FREQUENCIES,
+);
+pub const D_MINOR_SCALE: KeyScale = (
+    ["D", "E", "F", "G", "A", "Bb", "C"],
+    D_MINOR_SCALE_FREQUENCIES,
+);
+pub const G_MINOR_SCALE: KeyScale = (
+    ["G", "A", "Bb", "C", "D", "Eb", "F"],
+    G_MINOR_SCALE_FREQUENCIES,
+);
+pub const C_MINOR_SCALE: KeyScale = (
+    ["C", "D", "Eb", "F", "G", "Ab", "Bb"],
+    C_MINOR_SCALE_FREQUENCIES,
+);
+pub const F_MINOR_SCALE: KeyScale = (
+    ["F", "G", "Ab", "Bb", "C", "Db", "Eb"],
+    F_MINOR_SCALE_FREQUENCIES,
+);
+pub const BB_MINOR_SCALE: KeyScale = (
+    ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"],
+    BB_MINOR_SCALE_FREQUENCIES,
+);
+pub const EB_MINOR_SCALE: KeyScale = (
+    ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db"],
+    EB_MINOR_SCALE_FREQUENCIES,
+);
 
 /// =======================================
 /// All 24 Keys in One Array
 /// =======================================
 /// Each tuple is (KeyScale, KeyName).
 pub const KEYS: [(KeyScale, &'static str); 24] = [
-    (C_MAJOR_SCALE,        "C"),
-    (G_MAJOR_SCALE,        "G"),
-    (D_MAJOR_SCALE,        "D"),
-    (A_MAJOR_SCALE,        "A"),
-    (E_MAJOR_SCALE,        "E"),
-    (B_MAJOR_SCALE,        "B"),
-    (F_SHARP_MAJOR_SCALE,  "F#"),
-    (C_SHARP_MAJOR_SCALE,  "C#"),
-    (F_MAJOR_SCALE,        "F"),
-    (BB_MAJOR_SCALE,       "Bb"),
-    (EB_MAJOR_SCALE,       "Eb"),
-    (AB_MAJOR_SCALE,       "Ab"),
-
-    (A_MINOR_SCALE,        "A"),
-    (E_MINOR_SCALE,        "E"),
-    (B_MINOR_SCALE,        "B"),
-    (F_SHARP_MINOR_SCALE,  "F#"),
-    (C_SHARP_MINOR_SCALE,  "C#"),
-    (G_SHARP_MINOR_SCALE,  "G#"),
-    (D_MINOR_SCALE,        "D"),
-    (G_MINOR_SCALE,        "G"),
-    (C_MINOR_SCALE,        "C"),
-    (F_MINOR_SCALE,        "F"),
-    (BB_MINOR_SCALE,       "Bb"),
-    (EB_MINOR_SCALE,       "Eb"),
+    (C_MAJOR_SCALE, "C"),
+    (G_MAJOR_SCALE, "G"),
+    (D_MAJOR_SCALE, "D"),
+    (A_MAJOR_SCALE, "A"),
+    (E_MAJOR_SCALE, "E"),
+    (B_MAJOR_SCALE, "B"),
+    (F_SHARP_MAJOR_SCALE, "F#"),
+    (C_SHARP_MAJOR_SCALE, "C#"),
+    (F_MAJOR_SCALE, "F"),
+    (BB_MAJOR_SCALE, "Bb"),
+    (EB_MAJOR_SCALE, "Eb"),
+    (AB_MAJOR_SCALE, "Ab"),
+    (A_MINOR_SCALE, "A"),
+    (E_MINOR_SCALE, "E"),
+    (B_MINOR_SCALE, "B"),
+    (F_SHARP_MINOR_SCALE, "F#"),
+    (C_SHARP_MINOR_SCALE, "C#"),
+    (G_SHARP_MINOR_SCALE, "G#"),
+    (D_MINOR_SCALE, "D"),
+    (G_MINOR_SCALE, "G"),
+    (C_MINOR_SCALE, "C"),
+    (F_MINOR_SCALE, "F"),
+    (BB_MINOR_SCALE, "Bb"),
+    (EB_MINOR_SCALE, "Eb"),
 ];
 
 /// =======================================
@@ -85,9 +154,9 @@ pub fn get_note_name(note: i32, scale: KeyScale) -> &'static str {
         5 => scale.0[4],
         6 => scale.0[5],
         7 => scale.0[6],
-        8 => scale.0[0],  // wrap around
-        9 => scale.0[1],  // wrap around
-        _ => "",        // out of range
+        8 => scale.0[0], // wrap around
+        9 => scale.0[1], // wrap around
+        _ => "",         // out of range
     }
 }
 
@@ -131,12 +200,12 @@ pub fn get_key_name(key: i32) -> &'static str {
     }
 }
 
-pub fn get_scale_by_key(key:i32) -> &'static KeyScaleFrequencies {
+pub fn get_scale_by_key(key: i32) -> &'static KeyScaleFrequencies {
     let key = key as usize;
     if key < KEYS.len() {
-        &KEYS[key].0.1
+        &KEYS[key].0 .1
     } else {
         // Fallback to first key, or handle differently
-        &KEYS[0].0.1
+        &KEYS[0].0 .1
     }
 }
